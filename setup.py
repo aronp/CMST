894089aeb6d9f7944de5f41a5934fe745bd6244e
Aron <aron@pricingmonkey.com>
2026-06-06 22:35:21 +0100

    Added new functions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cmst-window",
-    version="0.1.0",
+    version="0.1.1",
     author="Aron Palmer",
     author_email="palmer.aron+cmst@gmail.com", 
     description="The analytically stable, zero-preserving, hyper-flat window.",
